a0c02e62ec5a9860586b87eebf6d163352660e52
Enrinal Zulhimar <muhamadenrinal@gmail.com>
2019-08-26 00:48:56 +0700

    ADD feature create order

---
+++ order-cli/cli.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"context"
+
+	pb "github.com/enrinal/hermes/order/proto/order"
+	"google.golang.org/grpc"
+)
+
+const (
+	address         = "localhost:50051"
+	defaultFilename = "order.json"
+)
+
+func parseFile(file string) (*pb.Order, error) {
+	var order *pb.Order
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	json.Unmarshal(data, &order)
+	return order, err
+}
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewOrderServiceClient(conn)
+
+	// Contact the server and print out its response.
+	file := defaultFilename
+	if len(os.Args) > 1 {
+		file = os.Args[1]
+	}
+
+	order, err := parseFile(file)
+
+	if err != nil {
+		log.Fatalf("Could not parse file: %v", err)
+	}
+
+	r, err := client.CreateOrder(context.Background(), order)
+	if err != nil {
+		log.Fatalf("Could not greet: %v", err)
+	}
+	log.Printf("Created: %t", r.Created)
+}
